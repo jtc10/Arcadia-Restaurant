fedc4556219c050330a4ad2bedbdf45904012f16
John Cyzeski <jcyzeski@gmail.com>
2018-10-03 18:13:33 -0400

    updates

--- script.js
+++ script.js
@@ -15,11 +15,11 @@ const image = [
   // '../images/karakoy-3.jpg',
   // '../images/karakoy-4.jpg',
   // '../images/karakoy-5.jpg',
-  '/images/karakoy-1.jpg',
-  '/images/karakoy-2.jpg',
-  '/images/karakoy-3.jpg',
-  '/images/karakoy-4.jpg',
-  '/images/karakoy-5.jpg',
+  './images/karakoy-1.jpg',
+  './images/karakoy-2.jpg',
+  './images/karakoy-3.jpg',
+  './images/karakoy-4.jpg',
+  './images/karakoy-5.jpg',
 ];
 let i = 0;
 const imageContainer = document.getElementsByClassName('slideshow-image');
